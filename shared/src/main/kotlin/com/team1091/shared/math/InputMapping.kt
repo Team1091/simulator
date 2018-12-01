08fc38f55ea9ec15d26a36ec211f3e38839f1cd3
Aut0P1ay <32904239+Aut0P1ay@users.noreply.github.com>
2018-12-01 09:13:48 -0600

    StretchType 1 Functional

--- shared/src/main/kotlin/com/team1091/shared/math/InputMapping.kt
+++ shared/src/main/kotlin/com/team1091/shared/math/InputMapping.kt
@@ -1,8 +1,50 @@
 package com.team1091.shared.math
 
-fun squareACircle(x: Double, y: Double): Pair<Double, Double> {
+fun squareACircle(u: Double, v: Double): Pair<Double, Double> {
+    var x = 0.0
+    var y = 0.0
+    var u2v2 = Math.pow(u, 2.0) + Math.pow(v, 2.0)
+    val stretchType = 1 //Stretch type 1 is currently functional, the others may not work.
+    if (stretchType == 1) {
+        var unchangedEquation = Math.sqrt(u2v2)
+        if (Math.pow(u, 2.0) >= Math.pow(v, 2.0)) {
+            x = Math.signum(u) * unchangedEquation
+        } else {
+            x = Math.signum(v) * (u / v) * unchangedEquation
+        }
+        if (Math.pow(u, 2.0) >= Math.pow(v, 2.0)) {
+            y = Math.signum(u) * (v / u) * unchangedEquation
+        } else {
+            y = Math.signum(v) * unchangedEquation
+        }
+        /*
+    } else if (stretchType == 2) {
+        var unchangedEquation = Math.sqrt(
+                u2v2 - Math.sqrt(
+                        u2v2 * (u2v2 - (4 * Math.pow(u, 2.0) * Math.pow(v, 2.0)))
+                )
+        )
+        x = Math.signum(u * v) / (v * Math.sqrt(2.0)) * unchangedEquation
+        y = Math.signum(u * v) / (u * Math.sqrt(2.0)) * unchangedEquation
+    } else if (stretchType == 3) {
+        x = .5 * Math.sqrt(
+                2 + Math.pow(u, 2.0) - Math.pow(v, 2.0) + 2 * Math.sqrt(2.0) * u
+        )
+        */
+    } else if (stretchType == 4) {
+        x = u
+        y = v
+    }
+    if(u == 0.0)
+    {
+        x = 0.0
+    }
+    if(v == 0.0)
+    {
+        y = 0.0
+    }
+    println("X Value: $x")
+    println("Y Value: $y")
     // https://arxiv.org/pdf/1509.06344.pdf
-
-    //TODO: math to stretch input
     return Pair(x, y)
 }
--- shared/src/test/kotlin/InputMappingTest.kt
+++ shared/src/test/kotlin/InputMappingTest.kt
@@ -1,8 +1,18 @@
 import com.team1091.shared.math.squareACircle
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import java.lang.Math.toRadians
+import kotlin.math.cos
+import kotlin.math.sin
 
 class InputMappingTest {
+
+    @Test
+    fun testInputSquaringAtZero() {
+        val (x, y) = squareACircle(0.0, 0.0)
+        assertEquals(0.0, x, 0.001)
+        assertEquals(0.0, y, 0.001)
+    }
 
     @Test
     fun testInputSquaringForward() {
@@ -18,12 +28,11 @@ class InputMappingTest {
         assertEquals(0.0, y, 0.001)
     }
 
-    // TODO: we need to make this work
-//    @Test
-//    fun testFortyFive() {
-//        val (x, y) = squareACircle(cos(toRadians(45.0)), sin(toRadians(45.0)))
-//        assertEquals(1.0, x, 0.001)
-//        assertEquals(1.0, y, 0.001)
-//    }
+    @Test
+    fun testFortyFive() {
+        val (x, y) = squareACircle(cos(toRadians(45.0)), sin(toRadians(45.0)))
+        assertEquals(1.0, x, 0.001)
+        assertEquals(1.0, y, 0.001)
+    }
 
 }
